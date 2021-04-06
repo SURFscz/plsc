5720c7d689b74277729d389d5660fafdcb2f1b8d
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2021-04-06 07:10:12 +0200

    restore full info for individual organizations and list of all services

--- sbs.py
+++ sbs.py
@@ -76,12 +76,10 @@ class SBS(object):
         return self.api('api/organisations/all')
 
     def organisation(self, org_id):
-        organisations = self.organisations()
-        for o in organisations:
-            if o['id'] == org_id:
-                return o
+        return self.api(f"api/organisations/{org_id}")
 
-        raise Exception(f"Organisation {id} not found !")
+    def services(self):
+        return self.api(f"api/services/all")
 
     def service(self, s_id):
         return self.api(f"api/services/{s_id}")
