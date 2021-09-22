ba796ec3009db7da02e18c2873af1218a2f632d5
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2021-09-22 12:10:39 +0200

    rewrite emails.py

--- emails.py
+++ emails.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
+import getopt
+import csv
 import yaml
+from typing import List, Dict
 from datetime import datetime
 from sbs import SBS
 
@@ -9,54 +12,78 @@ from sbs import SBS
 if len(sys.argv) < 2:
     sys.exit(sys.argv[0] + "  <conf.yml>")
 
-with open(sys.argv[1]) as f:
-    config = yaml.safe_load(f)
 
-src = SBS(config['sbs']['src'])
+contacts_type = List[Dict[str, str]]
 
-print("SRAM prod contacts generated " + datetime.now().isoformat())
-print("type:Name:id:role:email")
 
-organisations = src.organisations()
-#print(json.dumps(organisations, indent=2))
-#exit()
+def open_sbs(plsc_config_file: str) -> SBS:
+    with open(plsc_config_file) as f:
+        config = yaml.safe_load(f)
 
-for organisation in organisations:
-    o_id = organisation['id']
-    org = src.organisation(o_id)
-    name = org['name']
-    #print(json.dumps(org, indent=2))
-    #print(f"Organisation: {name}")
-    for user in org['organisation_memberships']:
-        role = user['role']
-        mail = user['user']['email']
-        print(f"org,{o_id},{name},org-{role},{mail}")
+    src = SBS(config['sbs']['src'])
+    return src
 
-services = src.services()
-#print("\nServices")
-#print(json.dumps(organisations, indent=2))
 
-for service in services:
-    s_id = service['id']
-    srvc = src.service(s_id)
-    #print(json.dumps(srvc, indent=2))
-    name = srvc['name']
-    mail = srvc['contact_email']
-    print(f"service,{s_id},{name},sp-contact,{mail}")
+def fetch_contacts(src: SBS, org: bool = True, co: bool = True, service: bool = True) -> contacts_type:
+    contacts: contacts_type = []
+    if org:
+        organisations = src.organisations()
+        for organisation in organisations:
+            o_id = organisation['id']
+            org = src.organisation(o_id)
+            for user in org['organisation_memberships']:
+                contacts.append({
+                    "type": "org",
+                    "id": o_id,
+                    "name": org['name'],
+                    "role": "org-" + user['role'],
+                    "mail": user['user']['email']
+                })
 
-collaborations = src.collaborations()
-#print("\nCollaborations")
-#print(json.dumps(collaborations, indent=2))
+    if service:
+        services = src.services()
+        for service in services:
+            s_id = service['id']
+            srvc = src.service(s_id)
+            contacts.append({
+                "type": "service",
+                "id": s_id,
+                "name": srvc['name'],
+                "role": "sp-contact",
+                "mail": srvc['contact_email']
+            })
 
-for collaboration in collaborations:
-    c_id = collaboration['id']
-    col = src.collaboration(c_id)
-    if col is None or not col:
-        continue
-    #print(json.dumps(col, indent=2))
-    name = col.get('name', '-')
-    for user in col['collaboration_memberships']:
-        role = user['role']
-        mail = user['user']['email']
-        if role == 'admin':
-            print(f"co,{c_id},{name},co-{role},{mail}")
+    if co:
+        collaborations = src.collaborations()
+        for collaboration in collaborations:
+            c_id = collaboration['id']
+            col = src.collaboration(c_id)
+            if col is None or not col:
+                continue
+            for user in col['collaboration_memberships']:
+                role = user['role']
+                if role == 'admin':
+                    contacts.append({
+                        "type": "co",
+                        "id": c_id,
+                        "name": col.get('name', '-'),
+                        "role": "co-" + role,
+                        "mail": user['user']['email']
+                    })
+
+    return contacts
+
+
+def main() -> None:
+    sbs = open_sbs(sys.argv[1])
+    contacts = fetch_contacts(sbs)
+
+    w = csv.writer(sys.stdout, dialect="excel")
+    w.writerow(["SRAM prod contacts generated " + datetime.now().isoformat()])
+    columns = ("type", "id", "name", "role", "mail")
+    w.writerow(columns)
+    w.writerows([[c[key] for key in columns] for c in contacts])
+
+
+if __name__ == "__main__":
+    main()
