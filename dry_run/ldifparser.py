de5fbcc234af1d68d56d8c17c135c6ba83326824
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2024-05-27 15:53:22 +0200

    ok, let's just duplicate the code then

--- dry_run/ldifparser.py
+++ dry_run/ldifparser.py
@@ -1,1 +1,41 @@
-../../SRAM-deploy/roles/ldap_monitor/files/ldifparser.py
+#!/usr/bin/env python3
+import sys
+import ldif
+from collections import OrderedDict
+
+
+def kcmp(item):
+    (key, v) = item
+    parts = key.split(',')[::-1]
+    new_key = ','.join(parts)
+    return (new_key, v)
+
+
+def freeze(o):
+    if isinstance(o, dict):
+        return OrderedDict({k: freeze(v) for k, v in sorted(o.items(), key=kcmp)}.items())
+    if isinstance(o, list):
+        return sorted([freeze(v) for v in o])
+    return o.decode('utf-8')
+
+
+def my_print(o, depth):
+    if isinstance(o, OrderedDict):
+        for k, v in o.items():
+            my_print(k, depth)
+            my_print(v, depth + 2)
+    elif isinstance(o, list):
+        for v in o:
+            my_print(v, depth)
+    else:
+        print(f"{' ' * depth}{o}")
+
+
+ldifparser = ldif.LDIFRecordList(sys.stdin)
+ldifparser.parse()
+
+data = {k: v for k, v in ldifparser.all_records}
+f = freeze(data)
+
+# print(json.dumps(f, indent=2))
+my_print(f, 0)
