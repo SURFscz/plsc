821ed1fb04a1553402445d48e47a9fcf5b3bc93c
Martin van Es <martin@mrvanes.com>
2024-09-24 10:11:37 +0200

    Fix CI action

---
+++ run-ldapci.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+docker rm ldap-ci || true
+docker run -ti \
+    -e "LDAP_SEED_INTERNAL_SCHEMA_PATH=/opt/misc/schema" \
+    -e "LDAP_DOMAIN=services.sram.tld" \
+    -e "LDAP_ADMIN_USERNAME=admin" \
+    -e "LDAP_ADMIN_PASSWORD=secret" \
+    -e "LDAP_CONFIG_PASSWORD=config" \
+    -e "LDAP_BASE_DN=dc=services,dc=sram,dc=tld" \
+    -e "LDAP_TLS=true" \
+    -v "./misc/schema:/opt/misc/schema" \
+    -p 389:389 \
+    --name ldap-ci \
+    osixia/openldap:latest
